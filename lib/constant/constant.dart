77b70bb08c7cf73b61cc3e238c12d8cba63d4646
MennaFathi <mnamohamed098@gmail.com>
2025-08-04 21:03:45 +0200

    edit kcolors

--- lib/constant/constant.dart
+++ lib/constant/constant.dart
@@ -1,4 +1,12 @@
-import 'package:flutter/material.dart';
+import 'dart:ui';
 
-const kPrimaryColor = Colors.cyanAccent;
-const knotesBox = 'notes_box';
+const kPrimaryColor = Color(0xff62FCD7);
+const kNotesBox = 'notes_box';
+List<Color> kColors = const [
+  Color(0xffFD99FF),
+  Color(0xffFF9E9E),
+  Color(0xff91F48F),
+  Color(0xffFFF599),
+  Color(0xff9EFFFF),
+  Color(0xffB69CFF),
+];
